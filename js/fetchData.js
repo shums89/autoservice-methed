a4e8b3bc0c219324535d6c271e2d063c8f4f6079
Aleksandr Shumatbaev <sh889@ya.ru>
2024-02-01 18:45:38 +0500

    feat: add backup data source

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,12 +1,17 @@
 export const fetchData = async () => {
   try {
-    const response = await fetch('https://planet-picayune-mousepad.glitch.me/api');
+    let response = await fetch('https://planet-picayune-mousepad.glitch.me/api');
 
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      response = await fetch('./data.json');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
     }
 
     return await response.json();
+
   } catch (error) {
     console.log(`Ошибка при получении данных: ${error}`);
   }
